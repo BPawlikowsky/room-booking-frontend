cc940189e174389297bf7ce173b01639ceda1759
Bartosz Pawlikowsky <bartoszpawlikowsky@gmail.com>
2019-12-07 18:46:40 +0100

    done

--- js/user.js
+++ js/user.js
@@ -1,44 +1,75 @@
-$.ajax({
-    type : 'GET',
-    url : 'http://localhost:8080/logged',
-    async : true,
-    cache : true,
-    headers : {
+var userID = 0;
+var userName = "";
+
+function logged() {
+  
+  return $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8080/logged',
+    async: true,
+    cache: true,
+    headers: {
       "Content-Type": "text/plain"
     },
-    success : function(resp) {
-      console.log(resp);
-      if(!resp.loggedIn) 
-      {
-        console.log("FALSE")
-        document.getElementById("user").innerHTML = "";
-        document.getElementById("userProfile").innerHTML = "";
-        document.getElementById("corporateProfile").innerHTML = "";
-      } else
-      {
-        console.log("TRUE");
-        document.getElementById("user").innerHTML = '<a onclick="logout();" href="index.html">' + resp.username + '</a>';
-        
-        if(resp.userType == "USER")
-          document.getElementById("userProfile").innerHTML = 
-                '<a href="user-profile.html">User Profile</a>';
-        if(resp.userType == "CORPO")
-           document.getElementById("corporateProfile").innerHTML = '<a href="corporate-user.html">Corporate Profile</a>';
-      }
+    success: function (callback) {
+      console.log(callback);
     }
   });
-  
-  function logout() {
-    $.ajax({
-        type : 'GET',
-        url : 'http://localhost:8080/logout/',
-        async : true,
-        cache : true,
-        headers : {
-          "Content-Type": "text/plain"
-        },
-        success : function(resp) {
-          console.log(resp);
-        }
-      });
+
+}
+
+function checkLogin(resp) {
+  if (!resp.loggedIn) {
+    console.log("FALSE")
+    document.getElementById("login").innerHTML = '<a href="login.html">Login</a>';
+    document.getElementById("register").innerHTML = '<a href="register.html">Register</a>';
+    document.getElementById("user").innerHTML = "";
+    document.getElementById("userProfile").innerHTML = "";
+    document.getElementById("corporateProfile").innerHTML = "";
+  } else {
+    console.log("TRUE " + resp.userId.toFixed());
+
+    userID = resp.userId.toFixed();
+    userName = resp.username;
+    
+    if (document.getElementById("wellcome") != null) document.getElementById("wellcome").innerHTML = "Wellcome " + userName + "!";
+    document.getElementById("user").innerHTML = '<a onclick="logout();" href="index.html">' + userName + '</a>';
+
+    switch (resp.userType.toString()) {
+      case 'USER': {
+        
+        document.getElementById("login").innerHTML = "";
+        document.getElementById("register").innerHTML = "";
+        document.getElementById("userProfile").innerHTML = '<a href="user-profile.html">User Profile</a>';
+      }
+        break;
+      case 'CORPO': {
+        
+        document.getElementById("login").innerHTML = "";
+        document.getElementById("register").innerHTML = "";
+        document.getElementById("corporateProfile").innerHTML = '<a href="corporate-user.html">Corporate Profile</a>';
+      }
+        break;
+    }
   }
+  userID = resp.userId;
+  console.log("user ID in response: " + resp.userId);
+  userName = resp.username;
+}
+
+logged().done(checkLogin).fail(function () { console.log("FAIL...");});
+  
+function logout() {
+  $.ajax({
+      type : 'GET',
+      url : 'http://localhost:8080/logout/',
+      async : true,
+      cache : true,
+      headers : {
+        "Content-Type": "text/plain"
+      },
+      success : function(resp) {
+        console.log(resp);
+      }
+    });
+}
