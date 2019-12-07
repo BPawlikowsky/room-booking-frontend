ad747c5d4cafbf08e2365f84e9ddb2c44f3adf56
Bartosz Pawlikowsky <bartoszpawlikowsky@gmail.com>
2019-12-07 21:05:11 +0100

    Everything worksgit add -A

--- js/corporate-profile.js
+++ js/corporate-profile.js
@@ -7,8 +7,35 @@ logged()
 		console.log("failed to get new user ID")
 	});
 
+function patchDataInApi(i, s) {
+	var json = {
+		reservationId: Number(i),
+		status: Number(s)
+	}
+	console.log(json)
+	$.ajax({
+			type: "PATCH",
+			url: "http://localhost:8080/reservation/status",
+			dataType: "json",
+			data: JSON.stringify(json),
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json"
+			}
+		})
+		.done(function (data, status) {
+			console.log("done")
+			window.location.reload(false);
+		})
+		.fail(function (data, status) {
+			console.log("fail")
+			window.alert("Can't delete this reservation.")
+		})
+}
+
 function postHotelDataToApi() {
 	var jsonData = {
+		name: $("#name").val(),
 		city: $("#city").val(),
 		street: $("#street").val(),
 		streetNumber: $("#streetNumber").val(),
@@ -46,64 +73,49 @@ function postHotelDataToApi() {
 			document.getElementById('error').style.display = "block",
 				document.getElementById('success').style.display = "none"
 		}
-	});
-}
-
-function deleteDataFromApi(i) {
-	$.ajax({
-		type: "DELETE",
-		url: "http://localhost:8080/reservation/?reservationId=" + i,
-		headers: {
-			"Content-Type": "application/json",
-			"Accepct": "application/json"
-		},
-		success: function () {
-			window.location.reload(false);
-		},
-		error: function () {
-			window.alert("Can't delete this reservation.")
-		}
 	})
 }
 
-var url = "http://localhost:8080/reservation?corporationId=1";
-
+var url = "http://localhost:8080/reservation/corporation?corporationId=1";
+console.log("dupa")
 $.getJSON(url, function (data) {
-
+	console.log(data)
 	for (var i = 0; i < data.length; i++) {
 
 		var roomType;
 		var roomTypeId;
+		var fullReservationPrice = data[i]["fullReservationPrice"];
+		var status = data[i]["status"];
 		if (roomTypeId % 2 == 0) {
 			roomType = "VIP"
 		} else {
 			roomType = "STANDARD"
 		};
 
-		var id = userID;
+		var id = data[i]["id"]
 
-		var yearStart = data[i]["reservationStart"]["0"] + data[i]["reservationStart"]["1"] + data[i][
-			"reservationStart"
-		]["2"] + data[i]["reservationStart"]["3"]
+		var yearStart = data[i]["reservationStart"]["0"] + data[i]["reservationStart"]["1"] + data[i]["reservationStart"]["2"] + data[i]["reservationStart"]["3"]
 		var monthStart = data[i]["reservationStart"]["5"] + data[i]["reservationStart"]["6"]
 		var dayStart = data[i]["reservationStart"]["8"] + data[i]["reservationStart"]["9"]
 
-		var yearEnd = data[i]["reservationEnd"]["0"] + data[i]["reservationEnd"]["1"] + data[i][
-			"reservationEnd"
-		]["2"] + data[i]["reservationEnd"]["3"]
+		var yearEnd = data[i]["reservationEnd"]["0"] + data[i]["reservationEnd"]["1"] + data[i]["reservationEnd"]["2"] + data[i]["reservationEnd"]["3"]
 		var monthEnd = data[i]["reservationEnd"]["5"] + data[i]["reservationEnd"]["6"]
 		var dayEnd = data[i]["reservationEnd"]["8"] + data[i]["reservationEnd"]["9"]
 
+		var accept = 1;
+		var reject = 2;
 		var tr = "<div class='table-row'>" +
 			"<div class='serial'>" + id + "</div>" +
-			"<div class='visit'>" + data[i]["hotelsId"] + "</div>" +
+			"<div class='visit'>" + data[i]["id"] + "</div>" +
 			"<div class='visit'>" + roomType + "</div>" +
 			"<div class='visit'>" + yearStart + "-" + monthStart + "-" + dayStart + "</div>" +
 			"<div class='visit'>" + yearEnd + "-" + monthEnd + "-" + dayEnd + "</div>" +
 			"<div class='visit'>" + roomType + "</div>" +
-			"<div class='visit'>" + roomType + "</div>" +
-			"<div class='visit'><input type='button' value='Delete' onclick='deleteDataFromApi(" + id +
-			")' class='genric-btn danger' /></div></div>";
+			"<div class='visit'>" + fullReservationPrice + "</div>" +
+			"<div class='visit'>" + status + "</div>" +
+			'<div class="visit"><input type="button" value="Accept" onclick="patchDataInApi(\'' + id + '\',\'' + accept + '\')" class="genric-btn success" /></div>' +
+			'<div class="visit"><input type="button" value="Reject" onclick="patchDataInApi(\'' + id + '\',\'' + reject + '\')" class="genric-btn danger"/></div>' +
+			"</div>";
 		$("#reservations").append(tr);
 	}
 });
