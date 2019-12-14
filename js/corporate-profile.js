cf1d3e86987b977b362f8619e87e3dc64e9c9c97
Marcin Stanisławek <mstanislawek@gmail.com>
2019-12-14 15:04:44 +0100

    Added dynamic url to get reservations for user and corpo

--- js/corporate-profile.js
+++ js/corporate-profile.js
@@ -76,45 +76,48 @@ function postHotelDataToApi() {
 	})
 }
 
-var url = "http://localhost:8080/reservation/corporation?corporationId=7";
-console.log("dupa")
-$.getJSON(url, function (data) {
-	console.log(data)
-	for (var i = 0; i < data.length; i++) {
+logged().done(function (mydata) {
+			var userIdurl = mydata["userId"]
 
-		var roomType;
-		var roomTypeId = data[i]["roomTypeId"];
-		var fullReservationPrice = data[i]["fullReservationPrice"];
-		var status = data[i]["status"];
-		if (roomTypeId % 2 != 0) {
-			roomType = "STANDARD"
-		} else {
-			roomType = "VIP"
-		};
+var url = "http://localhost:8080/reservation/corporation?corporationId=" + userIdurl;
+	$.getJSON(url, function (data) {
+		console.log(data)
+		for (var i = 0; i < data.length; i++) {
 
-		var id = data[i]["id"]
+			var roomType;
+			var roomTypeId = data[i]["roomTypeId"];
+			var fullReservationPrice = data[i]["fullReservationPrice"];
+			var status = data[i]["status"];
+			if (roomTypeId % 2 != 0) {
+				roomType = "STANDARD"
+			} else {
+				roomType = "VIP"
+			};
 
-		var yearStart = data[i]["reservationStart"]["0"] + data[i]["reservationStart"]["1"] + data[i]["reservationStart"]["2"] + data[i]["reservationStart"]["3"]
-		var monthStart = data[i]["reservationStart"]["5"] + data[i]["reservationStart"]["6"]
-		var dayStart = data[i]["reservationStart"]["8"] + data[i]["reservationStart"]["9"]
+			var id = data[i]["id"]
 
-		var yearEnd = data[i]["reservationEnd"]["0"] + data[i]["reservationEnd"]["1"] + data[i]["reservationEnd"]["2"] + data[i]["reservationEnd"]["3"]
-		var monthEnd = data[i]["reservationEnd"]["5"] + data[i]["reservationEnd"]["6"]
-		var dayEnd = data[i]["reservationEnd"]["8"] + data[i]["reservationEnd"]["9"]
+			var yearStart = data[i]["reservationStart"]["0"] + data[i]["reservationStart"]["1"] + data[i]["reservationStart"]["2"] + data[i]["reservationStart"]["3"]
+			var monthStart = data[i]["reservationStart"]["5"] + data[i]["reservationStart"]["6"]
+			var dayStart = data[i]["reservationStart"]["8"] + data[i]["reservationStart"]["9"]
 
-		var accept = 1;
-		var reject = 2;
-		var tr = "<div class='table-row'>" +
-			"<div class='serial'>" + id + "</div>" +
-			"<div class='visit'>" + data[i]["hotelsId"] + "</div>" +
-			"<div class='visit'>" + roomType + "</div>" +
-			"<div class='visit'>" + yearStart + "-" + monthStart + "-" + dayStart + "</div>" +
-			"<div class='visit'>" + yearEnd + "-" + monthEnd + "-" + dayEnd + "</div>" +
-			"<div class='visit'>" + fullReservationPrice + "</div>" +
-			"<div class='visit'>" + status + "</div>" +
-			'<div class="visit"><input type="button" value="Accept" onclick="patchDataInApi(\'' + id + '\',\'' + accept + '\')" class="genric-btn success" /></div>' +
-			'<div class="visit"><input type="button" value="Reject" onclick="patchDataInApi(\'' + id + '\',\'' + reject + '\')" class="genric-btn danger"/></div>' +
-			"</div>";
-		$("#reservations").append(tr);
-	}
+			var yearEnd = data[i]["reservationEnd"]["0"] + data[i]["reservationEnd"]["1"] + data[i]["reservationEnd"]["2"] + data[i]["reservationEnd"]["3"]
+			var monthEnd = data[i]["reservationEnd"]["5"] + data[i]["reservationEnd"]["6"]
+			var dayEnd = data[i]["reservationEnd"]["8"] + data[i]["reservationEnd"]["9"]
+
+			var accept = 1;
+			var reject = 2;
+			var tr = "<div class='table-row'>" +
+				"<div class='serial'>" + id + "</div>" +
+				"<div class='visit'>" + data[i]["hotelsId"] + "</div>" +
+				"<div class='visit'>" + roomType + "</div>" +
+				"<div class='visit'>" + yearStart + "-" + monthStart + "-" + dayStart + "</div>" +
+				"<div class='visit'>" + yearEnd + "-" + monthEnd + "-" + dayEnd + "</div>" +
+				"<div class='visit'>" + fullReservationPrice + "</div>" +
+				"<div class='visit'>" + status + "</div>" +
+				'<div class="visit"><input type="button" value="Accept" onclick="patchDataInApi(\'' + id + '\',\'' + accept + '\')" class="genric-btn success" /></div>' +
+				'<div class="visit"><input type="button" value="Reject" onclick="patchDataInApi(\'' + id + '\',\'' + reject + '\')" class="genric-btn danger"/></div>' +
+				"</div>";
+			$("#reservations").append(tr);
+		}
+	})
 });
--- js/user-profile.js
+++ js/user-profile.js
@@ -19,76 +19,78 @@ function deleteDataFromApi(i) {
 }
 
 
+logged().done(function (mydata) {
+	var userIdurl = mydata["userId"]
 
+var url = "http://localhost:8080/reservation?userId=" + userIdurl;
 
-var url = "http://localhost:8080/reservation?userId=1";
+	$.getJSON(url, function (data) {
+		for (var i = 0; i < data.length; i++) {
+			var roomTypeId = data[i]["roomTypeId"]
+			var roomType;
+			if (roomTypeId % 2 != 0) {
+				roomType = "STANDARD";
+			} else {
+				roomType = "VIP";
+			}
 
-$.getJSON(url, function (data) {
-	for (var i = 0; i < data.length; i++) {
-		var roomTypeId = data[i]["roomTypeId"]
-		var roomType;
-		if (roomTypeId % 2 != 0) {
-			roomType = "STANDARD";
-		} else {
-			roomType = "VIP";
+			var id = data[i]["id"];
+
+			var yearStart =
+				data[i]["reservationStart"]["0"] +
+				data[i]["reservationStart"]["1"] +
+				data[i]["reservationStart"]["2"] +
+				data[i]["reservationStart"]["3"];
+			var monthStart =
+				data[i]["reservationStart"]["5"] + data[i]["reservationStart"]["6"];
+			var dayStart =
+				data[i]["reservationStart"]["8"] + data[i]["reservationStart"]["9"];
+
+			var yearEnd =
+				data[i]["reservationEnd"]["0"] +
+				data[i]["reservationEnd"]["1"] +
+				data[i]["reservationEnd"]["2"] +
+				data[i]["reservationEnd"]["3"];
+			var monthEnd =
+				data[i]["reservationEnd"]["5"] + data[i]["reservationEnd"]["6"];
+			var dayEnd =
+				data[i]["reservationEnd"]["8"] + data[i]["reservationEnd"]["9"];
+
+			var tr =
+				"<div class='table-row'>" +
+				"<div class='serial'>" +
+				id +
+				"</div>" +
+				"<div class='visit'>" +
+				data[i]["hotelsId"] +
+				"</div>" +
+				"<div class='visit'>" +
+				roomType +
+				"</div>" +
+				"<div class='visit'>" +
+				yearStart +
+				"-" +
+				monthStart +
+				"-" +
+				dayStart +
+				"</div>" +
+				"<div class='visit'>" +
+				yearEnd +
+				"-" +
+				monthEnd +
+				"-" +
+				dayEnd +
+				"</div>" +
+				"<div class='visit'>" +
+				data[i]["fullReservationPrice"] +
+				"</div>" +
+				"<div class='visit'>" +
+				data[i]["status"] +
+				"</div>" +
+				"<div class='visit'><input type='button' value='Delete' onclick='deleteDataFromApi(" +
+				id +
+				")' class='genric-btn danger' /></div></div>";
+			$("#reservations").append(tr);
 		}
-
-		var id = data[i]["id"];
-
-		var yearStart =
-			data[i]["reservationStart"]["0"] +
-			data[i]["reservationStart"]["1"] +
-			data[i]["reservationStart"]["2"] +
-			data[i]["reservationStart"]["3"];
-		var monthStart =
-			data[i]["reservationStart"]["5"] + data[i]["reservationStart"]["6"];
-		var dayStart =
-			data[i]["reservationStart"]["8"] + data[i]["reservationStart"]["9"];
-
-		var yearEnd =
-			data[i]["reservationEnd"]["0"] +
-			data[i]["reservationEnd"]["1"] +
-			data[i]["reservationEnd"]["2"] +
-			data[i]["reservationEnd"]["3"];
-		var monthEnd =
-			data[i]["reservationEnd"]["5"] + data[i]["reservationEnd"]["6"];
-		var dayEnd =
-			data[i]["reservationEnd"]["8"] + data[i]["reservationEnd"]["9"];
-
-		var tr =
-			"<div class='table-row'>" +
-			"<div class='serial'>" +
-			id +
-			"</div>" +
-			"<div class='visit'>" +
-			data[i]["hotelsId"] +
-			"</div>" +
-			"<div class='visit'>" +
-			roomType +
-			"</div>" +
-			"<div class='visit'>" +
-			yearStart +
-			"-" +
-			monthStart +
-			"-" +
-			dayStart +
-			"</div>" +
-			"<div class='visit'>" +
-			yearEnd +
-			"-" +
-			monthEnd +
-			"-" +
-			dayEnd +
-			"</div>" +
-			"<div class='visit'>" +
-			data[i]["fullReservationPrice"] +
-			"</div>" +
-			"<div class='visit'>" +
-			data[i]["status"] +
-			"</div>" +
-			"<div class='visit'><input type='button' value='Delete' onclick='deleteDataFromApi(" +
-			id +
-			")' class='genric-btn danger' /></div></div>";
-		$("#reservations").append(tr);
-	}
+	})
 });
